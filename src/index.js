6fb0c817039d58a14fb4f8b9c385cd686539ab48
Madhav Paudel <poudelmadhav143@gmail.com>
2019-08-23 14:18:54 +0545

    initial language i18n implementated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import common_de from "./translations/de/common.json";
+import common_en from "./translations/en/common.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      en: {
+        translation: common_en
+      },
+      de: {
+        translation: common_de
+      },
+    },
+    lng: "de",
+    fallbackLng: "en",
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+function App() {
+  const { t } = useTranslation();
+
+  return <h2>{t('welcome.title', { framework: "react-i18next" })}</h2>;
+}
+
+// append app to dom
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
---
+++ src/welcome/hello.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HelloMessage extends Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}!
+      </div>
+    );
+  }
+}
+
+export default HelloMessage;
